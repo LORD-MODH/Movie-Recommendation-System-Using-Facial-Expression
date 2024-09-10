44e6120ac8879e40a8de43a3e7c5b0889eb355be
Lord-modh <111983731+LORD-MODH@users.noreply.github.com>
2024-09-10 13:40:25 +0530

    Update fer.py

--- fer.py
+++ fer.py
@@ -3,21 +3,23 @@ import streamlit as st
 from deepface import DeepFace
 
 def facial_analysis():
-  st.title("Webcam Live Feed with Facial Expression Analysis")
-  run = st.checkbox('Run the Model')
-  FRAME_WINDOW = st.image([])
-  if run:
-      camera = cv2.VideoCapture(0)
-      while run:
-          _, frame = camera.read()
-          frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-          try:
-              result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=True)
-              st.write(result)
-          except ValueError as e:
-              st.write("No face detected")
-          FRAME_WINDOW.image(frame)
-      camera.release()
-      st.write('Stopped')
-  else:
-      st.write("Click 'Run' to start the webcam.")
+    run = st.checkbox('Run Webcam')
+    FRAME_WINDOW = st.image([])
+    if run:
+        camera = cv2.VideoCapture(0)
+        while run:
+            ret, frame = camera.read()
+            if not ret:
+                st.warning("Failed to capture image from webcam. Please check if the webcam is accessible.")
+                break  
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            try:
+                result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
+                st.write(result['emotion'])
+            except ValueError:
+                st.write("No face detected")
+            FRAME_WINDOW.image(frame)
+        camera.release()
+        st.write("Webcam stopped.")
+    else:
+        st.write("Click 'Run Webcam' to start.")
